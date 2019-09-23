fbea5de1015c49d6ddce2a657b4f2ec5c53f5825
Максим Торжков <makstorzhkov@outlook.com>
2019-09-23 18:52:22 +0300

    added Lab_02

---
+++ scnd/Geom_Figure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scnd
+{
+    //....Абстрактный класс Геометрическая фигура....
+    abstract class Geom_Figure
+    {
+        public abstract double Area();
+    };
+}
---
+++ scnd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scnd
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Торжков Максим ИУ5-31Б";
+            Rectangle One = new Rectangle(3, 16); One.Print(); One.lngth = 15; One.wdth = 2; One.Print();
+
+            Square Two = new Square(8); Two.Print();
+
+            Circle Three = new Circle(9); Three.Print();
+
+            Console.ReadLine();
+        }
+    }
+}

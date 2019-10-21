eaed7fa6f3c13f55c66c18a5cf2fb4aacc369f66
Максим Торжков <makstorzhkov@outlook.com>
2019-10-21 21:59:38 +0300

    add Lab_04

---
+++ Lab_04/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Lab_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.textBoxReadTime = new System.Windows.Forms.TextBox();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.lableTimeRead = new System.Windows.Forms.Label();
+            this.labelWords = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelTimeSearch = new System.Windows.Forms.Label();
+            this.textBoxTimeSearch = new System.Windows.Forms.TextBox();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBoxSearch.Location = new System.Drawing.Point(383, 28);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBoxReadTime
+            // 
+            this.textBoxReadTime.Location = new System.Drawing.Point(156, 93);
+            this.textBoxReadTime.Name = "textBoxReadTime";
+            this.textBoxReadTime.ReadOnly = true;
+            this.textBoxReadTime.Size = new System.Drawing.Size(100, 20);
+            this.textBoxReadTime.TabIndex = 3;
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(156, 126);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.ReadOnly = true;
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 4;
+            this.textBoxCount.TextChanged += new System.EventHandler(this.TextBoxCount_TextChanged);
+            // 
+            // lableTimeRead
+            // 
+            this.lableTimeRead.AutoSize = true;
+            this.lableTimeRead.Location = new System.Drawing.Point(20, 93);
+            this.lableTimeRead.Name = "lableTimeRead";
+            this.lableTimeRead.Size = new System.Drawing.Size(77, 13);
+            this.lableTimeRead.TabIndex = 5;
+            this.lableTimeRead.Text = "Время чтения";
+            // 
+            // labelWords
+            // 
+            this.labelWords.AutoSize = true;
+            this.labelWords.Location = new System.Drawing.Point(20, 126);
+            this.labelWords.Name = "labelWords";
+            this.labelWords.Size = new System.Drawing.Size(93, 13);
+            this.labelWords.TabIndex = 6;
+            this.labelWords.Text = "Количество слов";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(528, 28);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // labelTimeSearch
+            // 
+            this.labelTimeSearch.AutoSize = true;
+            this.labelTimeSearch.Location = new System.Drawing.Point(380, 80);
+            this.labelTimeSearch.Name = "labelTimeSearch";
+            this.labelTimeSearch.Size = new System.Drawing.Size(85, 13);
+            this.labelTimeSearch.TabIndex = 8;
+            this.labelTimeSearch.Text = "Веремя поиска";
+            this.labelTimeSearch.Click += new System.EventHandler(this.LabelTimeSearch_Click);
+            // 
+            // textBoxTimeSearch
+            // 
+            this.textBoxTimeSearch.Location = new System.Drawing.Point(503, 77);
+            this.textBoxTimeSearch.Name = "textBoxTimeSearch";
+            this.textBoxTimeSearch.ReadOnly = true;
+            this.textBoxTimeSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTimeSearch.TabIndex = 9;
+            this.textBoxTimeSearch.TextChanged += new System.EventHandler(this.TextBoxTimeSearch_TextChanged);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(383, 143);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(220, 160);
+            this.listBoxResult.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.textBoxTimeSearch);
+            this.Controls.Add(this.labelTimeSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelWords);
+            this.Controls.Add(this.lableTimeRead);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.textBoxReadTime);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.TextBox textBoxReadTime;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Label lableTimeRead;
+        private System.Windows.Forms.Label labelWords;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelTimeSearch;
+        private System.Windows.Forms.TextBox textBoxTimeSearch;
+        private System.Windows.Forms.ListBox listBoxResult;
+    }
+}
+
---
+++ Lab_04/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Список слов
+        /// </summary>
+        
+        List<string> lst = new List<string>();
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fldlg = new OpenFileDialog();
+            fldlg.Filter = "Текстовый файл |*.txt";
+            if (fldlg.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch time = new Stopwatch();
+                time.Start();
+                string text = File.ReadAllText(fldlg.FileName);
+                char[] separ = { '\n', ' ', '.', ',', '\t', '/', '?' };
+                string[] words = text.Split(separ);
+                foreach(string word in words)
+                {
+                    string wrd = word.Trim();
+                    if (!lst.Contains(wrd)) lst.Add(wrd);
+                }
+                time.Stop();
+                this.textBoxReadTime.Text = time.Elapsed.ToString();
+                this.textBoxCount.Text = lst.Count().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл.");
+            }
+            
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxCount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            string word = textBoxSearch.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && lst.Count > 0)
+            {
+                word = word.ToUpper();
+                List<string> res = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string wrd in lst)
+                {
+                    if (wrd.ToUpper().Contains(word))
+                    {
+                        res.Add(wrd);
+                    }
+                }
+                t.Stop();
+                this.textBoxTimeSearch.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+
+                foreach(string wrd in res)
+                {
+                    this.listBoxResult.Items.Add(wrd);
+                }
+                this.listBoxResult.EndUpdate();
+
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+        private void LabelTimeSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxTimeSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

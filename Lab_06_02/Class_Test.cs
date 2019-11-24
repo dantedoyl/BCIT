4a1790a2253bf338be5ce3261688416bdb4ec3b3
Максим Торжков <makstorzhkov@outlook.com>
2019-11-24 15:50:41 +0300

    Part 2 of Lab06

---
+++ Lab_06_02/Class_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06_02
+{
+    class Class_Test
+    {
+        public Class_Test() { }
+        public Class_Test(int i) { }
+        public Class_Test(string str) { }
+
+        public int Plus(int x, int y) { return x + y; }
+        public int Minus(int x, int y) { return x - y; }
+
+        [MyAttrib("Описание для property1")]
+        public string property1
+        {
+            get { return _property1; }
+            set { _property1 = value; }
+        }
+        private string _property1;
+
+        public int property2 { get; set; }
+
+       [MyAttrib(Description = "Описание для property3")]
+        public double property3 { get; private set; }
+
+
+        public int field1;
+        public float field2;
+    }
+}
+
---
+++ Lab_06_02/MyAttrib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06_02
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class MyAttrib: Attribute
+    {
+        public MyAttrib() { }
+        public MyAttrib(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+
+        public string Description { get; set; }
+    }
+}
